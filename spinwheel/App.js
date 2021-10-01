c2bb7d39c10f483fb24bca1ba3e7023a93588d16
remagart <jfmamjjasond418@gmail.com>
2021-10-01 22:03:29 +0800

    UI

--- spinwheel/App.js
+++ spinwheel/App.js
@@ -1,112 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { } from 'react';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const {width: ScreenWidth, height: ScreenHeight} = Dimensions.get('screen');
+const ONE_WIDTH = ScreenWidth / 5;
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  function renderItem(key) {
+    return (
+      <View key={key} style={styles.itemView}>
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+      </View>
+    );
+  }
+
+  function renderRow() {
+    const arr = new Array(5).fill(0);
+    return (
+      <View style={styles.rowView}>
+        {arr.map((item, index) => {
+          return renderItem(`row-${index}`);
+        })}
+      </View>
+    );
+  }
+
+  function renderColumn(isLeft = true) {
+    const posStyle = isLeft ? {left: 0} : {right: 0};
+    const arr = new Array(5).fill(0);
+    return (
+      <View style={[styles.columnView, posStyle]}>
+        {arr.map((item, index) => {
+          return renderItem(`col-${index}`);
+        })}
+      </View>
+    );
+  }
+
+  function renderContainer() {
+    return (
+      <View style={styles.containerView}>
+        {renderRow()}
+        {renderColumn()}
+        <View style={{flex: 1}} />
+        {renderRow()}
+        {renderColumn(false)}
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      {renderContainer()}
+      <Text>123</Text>
     </View>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  containerView: {
+    width: ScreenWidth,
+    height: ScreenWidth,
+    backgroundColor: 'red',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  rowView: {
+    width: ScreenWidth,
+    height: ONE_WIDTH,
+    backgroundColor: 'blue',
+    flexDirection: 'row',
   },
-  highlight: {
-    fontWeight: '700',
+  columnView: {
+    width: ONE_WIDTH,
+    height: ScreenWidth,
+    backgroundColor: 'yellow',
+    position: 'absolute',
+  },
+  itemView: {
+    width: ONE_WIDTH,
+    height: ONE_WIDTH,
+    borderEndColor: '#000',
+    borderEndWidth: 1,
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
   },
 });
-
-export default App;
