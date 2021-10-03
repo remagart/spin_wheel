600b6b276acdd7bd6c7b1cc80f4781929a6013f7
remagart <jfmamjjasond418@gmail.com>
2021-10-03 14:47:56 +0800

    practiceBasic

--- spin_wheel_flutter/lib/main.dart
+++ spin_wheel_flutter/lib/main.dart
@@ -23,17 +23,93 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        alignment: Alignment.topCenter,
-        color: Colors.blue[300],
-        child: const Text('123'),
-        // ignore: prefer_const_constructors
-        constraints: BoxConstraints(
-            maxWidth: 300, maxHeight: 300, minWidth: 50, minHeight: 50),
-        margin: const EdgeInsets.all(50),
-        padding: const EdgeInsets.only(left: 50, top: 200),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.network(
+          'https://s.zimedia.com.tw/s/CjhG56-1',
+          width: 100,
+          height: 50,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 16),
+          color: Colors.amber[300],
+          child: const Text(
+            '1\n2\n3\n4',
+            style: TextStyle(
+                fontSize: 30,
+                color: Colors.black12,
+                decoration: TextDecoration.lineThrough,
+                fontWeight: FontWeight.bold),
+            maxLines: 3,
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 16),
+          // color: Colors.brown[300],
+          child: ElevatedButton(
+            onPressed: () {
+              onClicked(context);
+            },
+            style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(
+                    fontSize: 24, backgroundColor: Colors.yellow)),
+            child: const Text(
+              'press',
+            ),
+          ),
+        ),
+        Image.asset(
+          'lib/img/cat.jpeg',
+          width: 200,
+          height: 100,
+        )
+      ],
+    );
+  }
+
+  void onClicked(BuildContext context) {
+    print('dd');
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => PageB(testint: 'rrr')));
+  }
+}
+
+class PageB extends StatelessWidget {
+  String testint;
+  PageB({Key? key, this.testint = 'asd'}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(testint),
       ),
+      body: _PageB(testint: testint),
+    );
+  }
+}
+
+class _PageB extends StatelessWidget {
+  String testint;
+  _PageB({Key? key, this.testint = 'qqq'}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('back')),
+        Row(
+          children: [Text(testint)],
+        )
+      ],
     );
   }
 }
